891b8b35d453d2062ff51b19e31a5ee9b83106d8
angeltg <angeltellezgonzalez@gmail.com>
2019-06-07 17:02:35 +0200

    Grabamos la tarea en la bbdd

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.addTask = this.addTask.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    addTask(e){
+        fetch('/api/tasks', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            //promesa
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+
+        e.preventDefault();
+    }
+    handleChange(e){
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render(){
         return (
             <div>
@@ -16,15 +50,15 @@ class App extends Component {
                         <div className="col s5">
                             <div className="card">
                                 <div className="card-content">
-                                    <form>
+                                    <form onSubmit={this.addTask}>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input type="text" name="title" placeholder="Task título" />
+                                                <input type="text" name="title" onChange={this.handleChange} placeholder="Task título" />
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <textarea placeholder="Task description" className="materialize-textarea">
+                                                <textarea placeholder="Task description" onChange={this.handleChange} name="description" className="materialize-textarea">
 
                                                 </textarea>
                                             </div>
